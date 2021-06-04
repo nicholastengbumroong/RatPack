ec106210b3b3c1a5ecb006214279510f6f3fb701
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-06-04 01:30:55 -0700

    Intial creation of backend

--- client/client.js
+++ client/client.js
@@ -2,9 +2,13 @@ console.log("Hello World");
 
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading'); 
+const squeaksElement = document.querySelector('.squeaks')
+const API_URL = 'http://localhost:5000/squeaks';
 
-loadingElement.style.display = 'none'; 
 
+loadingElement.style.display = ''; 
+
+listAllSqueaks();
 
 form.addEventListener('submit', (event) => {
     event.preventDefault(); 
@@ -20,4 +24,51 @@ form.addEventListener('submit', (event) => {
     console.log(squeak);
     form.style.display = 'none'; 
     loadingElement.style.display = ''; 
-});  
+
+    fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(squeak),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+      .then(createdSqueak => {
+        // when finished submitting, re-display form
+        form.reset();
+        form.style.display = ''; 
+        listAllSqueaks();
+        loadingElement.style.display = 'none'; 
+      });
+}); 
+
+function listAllSqueaks() {
+    squeaksElement.innerHTML = '';
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(squeaks => {
+            console.log(squeaks);
+            squeaks.reverse(); 
+            squeaks.forEach(squeak => {
+                const div = document.createElement('div'); 
+
+                const header = document.createElement('h3'); 
+                header.textContent = squeak.name;
+
+                const contents = document.createElement('p');
+                contents.textContent = squeak.content; 
+
+                const date = document.createElement('small'); 
+                date.textContent = new Date(squeak.created); 
+
+                div.appendChild(header); 
+                div.appendChild(contents); 
+                div.appendChild(date); 
+                
+
+                squeaksElement.appendChild(div);
+            });
+            loadingElement.style.display = 'none'; 
+        });
+             
+        
+}
