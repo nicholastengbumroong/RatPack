79aaa5edaf71b021ece0fa9dbcb0d9f44c4bd6b9
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-06-04 21:48:44 -0700

    created mongodb connection

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 const express = require('express'); 
 const cors = require('cors'); 
+const mongoose = require('mongoose'); 
+const path = require('path');
+require('dotenv').config();
 
 const app = express(); 
 
 const http = require('http'); 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
-  });
-  
-  server.listen(port,() => {
-    console.log(`Server running at port `+port);
-  });
+const dbConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/ratpack'
+
+
+//connecting to the database
+mongoose.connect(dbConnection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//schema
+const Schema = mongoose.Schema;
+const squeakFormSchema = new Schema({
+    name: String,
+    content: String,
+    date: {
+        type: String, 
+        default: Date.now()
+    }
+});
+
+//model 
+const Squeak = mongoose.model('Squeak', squeakFormSchema); 
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected'); 
+});
+
+app.listen(port, () => {
+    console.log('Listening on port', port); 
+});
 
 /*
 const squeaks = db.get('squeaks'); 
@@ -65,9 +89,5 @@ app.post('/squeaks', (request, response) => {
         });    
     }
 });
-
-app.listen(5000, () => {
-    console.log('Listening on http://localhost:5000');
-});
-
 */
+
