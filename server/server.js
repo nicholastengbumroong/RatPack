e1860bf36a8283cda1487e80644dfdcd3f925582
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-06-05 01:40:41 -0700

    Added axios

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const cors = require('cors'); 
 const mongoose = require('mongoose'); 
+const path = require('path'); 
 
 require('dotenv').config();
 
@@ -21,6 +22,8 @@ mongoose.connection.on('connected', () => {
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 const squeaksRouter = require('./routes/squeaks');
 app.use('./squeaks', squeaksRouter);
