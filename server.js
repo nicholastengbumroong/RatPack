9a7de5bfb39e1e29dab36932ee3132ae8280aa55
Nicholas Tengbumroong <Cherryappletruck@gmail.com>
2021-06-15 01:48:45 -0700

    Hid mongodb_uri using dotenv

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors'); 
 const path = require('path');
+require('dotenv').config()
 
 const app = express(); 
 const port = process.env.PORT || 5000; 
 
 const routes = require('./routes/api');
 
-// will need to hide using dotenv 
-const MONGODB_URI = "mongodb+srv://NickTeng:August1900!@cluster0.6zugf.mongodb.net/RatPack?retryWrites=true&w=majority"
-const conn = MONGODB_URI || 'mongodb://localhost/5000/ratpack'
+const conn = process.env.MONGODB_URI || 'mongodb://localhost/5000/ratpack';
 
 // connect to the mongo database
 mongoose.connect(conn, {
